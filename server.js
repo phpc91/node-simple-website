48d0ffd1bbddbe5d364bbbecb2cbeb5ba4e6dec3
Ph <ph-pc@hotmail.com>
2018-05-27 15:47:56 -0300

    updates para testar ad banners deste mesmo commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express'),
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname))
 
 app.get('/', function(req, res, next){
     try {
@@ -26,6 +27,22 @@ app.get('/news', function(req, res, next){
     }
 })
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+app.get('/banner1', function(req, res, next){
+    try {
+        res.sendFile('/static/html/270x360.html', {root: __dirname})
+    } catch(e) {
+        next(e)
+    }
+});
+
+app.get('/banner2', function(req, res, next){
+    try {
+        res.sendFile('/static/html/1164x100.html', {root: __dirname})
+    } catch (e) {
+        next(e)
+    }
+});
+
+app.listen(process.env.PORT || 80, function(){
+    console.log('Listening on http://localhost:' + (process.env.PORT || 80))
 })
